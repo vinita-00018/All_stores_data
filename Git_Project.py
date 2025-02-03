55a683bb16ec5b4bfe98f121b71803798ec0e627
Vinita Pandla <vinitapandla.ecommerce@gmail.com>
2025-02-03 15:25:08 +0530

    Initial commit or your custom message

---
+++ Git_Project.py
@@ -0,0 +1,26 @@
+import subprocess
+import os
+
+try:
+    folder_path = "D:\\Vinita\\Project\\Final_Project_Streamlit"
+    # repository_url = "https://github.com/vinita-00018/Testing.git"
+    repository_url = "https://github.com/vinita-00018/Fetcher_data.git"
+    os.chdir(folder_path)
+    # Ensure Git is initialized
+    subprocess.run(["git", "init"], check=True)
+    # Check if 'origin' remote already exists
+    result = subprocess.run(["git", "remote"], capture_output=True, text=True)
+    if "origin" not in result.stdout:
+        subprocess.run(["git", "remote", "add", "origin", repository_url], check=True)
+    else:
+        print("Remote 'origin' already exists. Skipping addition.")
+    subprocess.run(["git", "add", "."], check=True)
+    commit_message = "Initial commit or your custom message"
+    subprocess.run(["git", "commit", "-m", commit_message], check=True)
+    subprocess.run(["git", "push", "-u", "origin", "main"], check=True)
+    print("Folder has been successfully pushed to GitHub!")
+
+except subprocess.CalledProcessError as e:
+    print(f"Git command failed: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
